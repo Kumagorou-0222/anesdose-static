443f1a4d31f5b6c630e6c461753e4bf9268fea0f
Kumagorou-0222 <2.64155347e+08+Kumagorou-0222@users.noreply.github.com>
2026-08-15 22:46:46 +0000

    publish: AnesDose static bundle from f33c683

--- assets/ReferencesTab-BZO9M0QA.js
+++ assets/ReferencesTab-BZO9M0QA.js
@@ -1,4 +1,4 @@
-import{c as x,r as m,j as e,a1 as p,a0 as i}from"./index-C7aKAN7Z.js";import{C as u}from"./chevron-down-S_AAtPQA.js";/**
+import{c as x,r as m,j as e,a1 as p,a0 as i}from"./index-DnHrgYjD.js";import{C as u}from"./chevron-down-BLLA9QXt.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
