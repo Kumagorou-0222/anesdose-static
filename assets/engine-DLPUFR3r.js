f9bdb45b8ac8629aad0512466d37ebe4520c4312
Kumagorou-0222 <2.64155347e+08+Kumagorou-0222@users.noreply.github.com>
2026-08-15 22:12:16 +0000

    publish: AnesDose static bundle from 53ffa71

--- assets/engine-DLPUFR3r.js
+++ assets/engine-DLPUFR3r.js
@@ -1,4 +1,4 @@
-import{c as v}from"./index-DqpFNRhr.js";/**
+import{c as v}from"./index-1mWAo2jO.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
